7e5a27d692057a98efeb8c22ca565aba551ef6f9
Alexandre <pcraft@outlook.fr>
2018-03-22 14:51:17 +0100

    Fonctionne

---
+++ MaureLCD.ino
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+
+void setup()
+{
+	Serial.begin(115200);
+}
+
+void loop()
+{
+	while (Serial.read() != 'T'); // On attend d'avoir T
+
+    int index = Serial.read();
+    if (index >= 80) return; // Hors limite
+    // move cursor to index
+    while(true) {
+        char c = Serial.read();
+        if (c == -1) continue; // Pas de caractère lu, on retente
+        if (c == 0) break; // Caractère de stop : on sort de la fonction
+        // print c
+    }
+}
